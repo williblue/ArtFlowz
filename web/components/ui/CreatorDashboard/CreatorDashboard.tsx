38130f876033f6f859cb31f56758423c2d957e17
williblue <90781813+williblue@users.noreply.github.com>
2023-02-28 05:04:30 +0100

    CreatorsDashboard Table

--- web/components/ui/CreatorDashboard/CreatorDashboard.tsx
+++ web/components/ui/CreatorDashboard/CreatorDashboard.tsx
@@ -2,6 +2,8 @@ import { FC } from "react"
 import styled from "styled-components"
 import BannerImg from "/public/dashboardbanner.png"
 import DalleImg from "/public/dalle.jpg"
+import CreatorsTable from "../CreatorsTable"
+import { useUser } from "@components/user/UserProvider"
 
 const Container = styled.div`
   margin: 0 auto;
@@ -17,7 +19,11 @@ const Container = styled.div`
     height: min-content;
   }
   font-family: "Sinhala MN", Arial, sans-serif;
-  background-color: rgba(250, 250, 250, 1);
+  background: linear-gradient(
+    to bottom,
+    #ffffff,
+    #d8cbed
+  ); /* Sets the linear gradient background color */
   height: 100vh;
 `
 
@@ -27,7 +33,7 @@ const ProfileCardContainer = styled.div`
   border-radius: 15px;
   overflow: visible;
   width: 480px;
-  height: 700px;
+  height: 650px;
   border-radius: 15px;
   filter: drop-shadow(0 1px 6px rgba(0, 0, 0, 0.161));
   display: flex;
@@ -55,8 +61,8 @@ const BannerImage = styled.div`
 
 const DashboardHeaderContainer = styled.div`
   display: block;
-  background: red;
   width: 100%;
+  padding: 40px 100px;
 `
 
 const DashboardHeader = styled.div`
@@ -69,29 +75,72 @@ const DashboardHeader = styled.div`
 `
 
 const Wrapper = styled.div`
-  background-color: black;
   display: flex;
   width: 100%;
 `
+const Title = styled.div`
+  font-size: 24px;
+  font-weight: bold;
+  margin-top: 20px;
+`
+
+const Subtitle = styled.div`
+  font-size: 18px;
+  font-weight: bold;
+  margin-top: 10px;
+  color: #9c9c9c;
+`
+
+const Total = styled.div`
+  font-size: 24px;
+  font-weight: bold;
+  margin-top: 40px;
+`
+
+const TotalSales = styled(Subtitle)``
+
+const DashboardText = styled(Subtitle)`
+  margin-top: 40px;
+`
+
+const Requests = styled(Subtitle)`
+  margin-top: 20px;
+`
+
+const H2 = styled.h2`
+  font-size: 1.7rem;
+  font-weight: normal;
+  margin-bottom: 40px;
+  margin-top: 60px;
+`
 
 const CreatorDashboard: FC = () => {
+  const { allCommissions } = useUser()
   return (
     <Container>
       <BannerImage />
       <Wrapper>
         <ProfileCardContainer>
           <CreatorImage src={DalleImg.src} />
-          <div>dalle.find</div>
-          <div>@dalle e</div>
-          <div>32,212</div>
-          <div>USDC</div>
-          <div>Total Sales</div>
-          <div>Dashboard</div>
-          <div>Requests</div>
+
+          <Title>Dalle Find</Title>
+          <Subtitle>@dalle_e</Subtitle>
+          <Total>32,212 USDC</Total>
+          <TotalSales>Total Sales</TotalSales>
+          <DashboardText>Dashboard</DashboardText>
+          <Requests>Requests</Requests>
         </ProfileCardContainer>
 
         <DashboardHeaderContainer>
           <DashboardHeader>Dashboard</DashboardHeader>
+          <H2>New Requests</H2>
+          <CreatorsTable
+            data={allCommissions}
+            select={undefined}
+            handleButtonClick={function (): void {
+              throw new Error("Function not implemented.")
+            }}
+          />
         </DashboardHeaderContainer>
       </Wrapper>
     </Container>
---
+++ web/components/ui/CreatorsTable/CreatorsTable.tsx
@@ -0,0 +1,79 @@
+import { FC } from "react"
+import styled from "styled-components"
+
+type Props = {
+  data: any
+  select: any
+  handleButtonClick: () => void
+}
+
+const Table = styled.table`
+  border-collapse: collapse;
+  width: 100%;
+  max-width: 1000px;
+`
+
+const TableHead = styled.thead`
+  background-color: #7539d4;
+  color: white;
+`
+
+const TableRow = styled.tr`
+  border-bottom: 1px solid #ccc;
+`
+
+const TableCell = styled.td`
+  padding: 10px;
+  text-align: center;
+`
+
+const Button = styled.button`
+  background-color: #7539d4;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 4px;
+  cursor: pointer;
+`
+
+const CreatorsTable: FC<Props> = ({ data, select, handleButtonClick }) => {
+  return (
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>Request ID</TableCell>
+          <TableCell>Offer Amount (USDC)</TableCell>
+          <TableCell>Commissioner</TableCell>
+          <TableCell>Genre</TableCell>
+          <TableCell>Status</TableCell>
+          <TableCell>Action</TableCell>
+        </TableRow>
+      </TableHead>
+      <tbody>
+        {data?.map((item: any) => (
+          <TableRow key={item.commissionID}>
+            <TableCell>{item.commissionID}</TableCell>
+            <TableCell>
+              {parseFloat(item.commissionAmount).toFixed(2)}
+            </TableCell>
+            <TableCell>{item.commissionerAddress}</TableCell>
+            <TableCell>{item.genre}</TableCell>
+            <TableCell>{item.status}</TableCell>
+            <TableCell>
+              <Button
+                onClick={() => {
+                  select(item)
+                  handleButtonClick()
+                }}
+              >
+                View
+              </Button>
+            </TableCell>
+          </TableRow>
+        ))}
+      </tbody>
+    </Table>
+  )
+}
+
+export default CreatorsTable
